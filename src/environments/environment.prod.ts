1d149d4d08549c23ae4eafbb44a46c3d2a95db0d
DanielTecalero <daniel.jimenezte@alumno.buap.mx>
2025-12-04 23:51:08 -0600

    Sera 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // ¡CAMBIA ESTA URL! Debe ser tu dominio PythonAnywhere con HTTPS
-  url_api: 'https://DanielT.pythonanywhere.com/',
+  url_api: 'https://DanielT.pythonanywhere.com',
 };
